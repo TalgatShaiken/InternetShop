ad612b455b6126122bc9c164dae746ebad0b8501
Шайкен Талгат Серікұлы <SHAYKETA@bank.corp.centercredit.kz>
2024-03-28 08:11:27 +0500

    Basket created

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Basket vasyaBasket = new Basket();
+        Basket petyaBasket = new Basket();
+        vasyaBasket.add("Молоко", 80);
+        vasyaBasket.add("Хлеб", 40);
+        petyaBasket.add("Лопата", 280);
+        petyaBasket.add("Бочка", 2900);
+        vasyaBasket.print("Корзина Васи: ");
+        System.out.println("Общая стоимость корзины: " + vasyaBasket.getTotalPrice());
+        System.out.println();
+        petyaBasket.print("Корзина Пети: ");
+        System.out.println("Общая стоимость корзины: " + petyaBasket.getTotalPrice());
     }
 }
